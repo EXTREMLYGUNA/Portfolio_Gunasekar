4514fd1a26496b218506525ed56684a01382fb81
GUNASEKAR <gunasekar17329777557amma@gmail.com>
2025-02-15 21:21:59 +0530

    Online-Resume

--- src/components/Blank.jsx
+++ src/components/Blank.jsx
@@ -5,16 +5,15 @@ import {Link} from 'react-router-dom'
 function Blank() {
   return (
     <>
-
-  <Link >
+  <Link t0='/blank' >
   <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj_DFgz08ZvaJsANrvNyoqZ_4NhdiEBOapTpa3oKPrFanJhiG6Mw50SaqVj8ho9wS3dSpfKzrci8HYSKdRKiSbMfWvvgC3mCMRbEHBZb6NqnDuIBFGdbcuHjNTOd0Lqtz5dd7XrR5afr_KdkiQZhMyHt_Ik4XnTVHbENpNxkvN0zhNGxn19y1fwlFFTZI0/s825/Resume1.jpg" />
   </Link>
  
-  <Link >
+  <Link to='/blank' >
   <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEii5ueqHIuqUh2R7gfoc_RYJ4gRhFokI1P9l4XNvnl2YQba_aB5afTIItIij8PyBs9eS4R2acgHRYoOKqeZhD3GddvK3roBgxhPZoc9KCap3eX23TMydwmH9VqrHX3yN8udxySTKZXw02U6bTsxRhUMZTdZQZmZHfa7aA7z_45xWSz3MOW3Pi_Ijj8ShGI/s826/Resume2.jpg" />
   </Link>
 
-  <Link >
+  <Link to='blank' >
   <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg_TYRGqnt4Lda6WLMGVA6cmxvxjyEXWmiqVRxk6q7MVyYe8xrp9oy2dQerrIhgPXe8nqx1moYaR3VK9ZczNhuC8m0sqCq6Apl-DfTgF54Aj1sp93bqvkz0W2_z5P-s9Od_FnLrhT5KRj5Vx82VKh1xawk7iguNNPyw-owR4LdYU8y0J9YBcMEgkx5zKec/s827/Resume3.jpg" />
   </Link>
 
