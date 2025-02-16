6c77787915b169c037db0a02262b0cbec023f9e0
GUNASEKAR <gunasekar17329777557amma@gmail.com>
2025-02-16 19:13:28 +0530

    Online-Resume

--- src/components/Blank.jsx
+++ src/components/Blank.jsx
@@ -5,31 +5,12 @@ import {Link} from 'react-router-dom'
 function Blank() {
   return (
     <>
-  <Link to="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj_DFgz08ZvaJsANrvNyoqZ_4NhdiEBOapTpa3oKPrFanJhiG6Mw50SaqVj8ho9wS3dSpfKzrci8HYSKdRKiSbMfWvvgC3mCMRbEHBZb6NqnDuIBFGdbcuHjNTOd0Lqtz5dd7XrR5afr_KdkiQZhMyHt_Ik4XnTVHbENpNxkvN0zhNGxn19y1fwlFFTZI0/s825/Resume1.jpg" alt='_Resume1' className='online' target='_blank' >
-  <h1>OPEN
-  <h3>Resume-Slide
-  <h4>1/3</h4><br/>
-  </h3><br/>
-  </h1><br/>
+  <Link >
+  <img title="resume" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj_DFgz08ZvaJsANrvNyoqZ_4NhdiEBOapTpa3oKPrFanJhiG6Mw50SaqVj8ho9wS3dSpfKzrci8HYSKdRKiSbMfWvvgC3mCMRbEHBZb6NqnDuIBFGdbcuHjNTOd0Lqtz5dd7XrR5afr_KdkiQZhMyHt_Ik4XnTVHbENpNxkvN0zhNGxn19y1fwlFFTZI0/s825/Resume1.jpg" alt='_Resume1' className='online' target='_blank' />
+  <img title="resume" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEii5ueqHIuqUh2R7gfoc_RYJ4gRhFokI1P9l4XNvnl2YQba_aB5afTIItIij8PyBs9eS4R2acgHRYoOKqeZhD3GddvK3roBgxhPZoc9KCap3eX23TMydwmH9VqrHX3yN8udxySTKZXw02U6bTsxRhUMZTdZQZmZHfa7aA7z_45xWSz3MOW3Pi_Ijj8ShGI/s826/Resume2.jpg" alt='_Resume2' className='online' target='_blank' />
+  <img title="resume" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg_TYRGqnt4Lda6WLMGVA6cmxvxjyEXWmiqVRxk6q7MVyYe8xrp9oy2dQerrIhgPXe8nqx1moYaR3VK9ZczNhuC8m0sqCq6Apl-DfTgF54Aj1sp93bqvkz0W2_z5P-s9Od_FnLrhT5KRj5Vx82VKh1xawk7iguNNPyw-owR4LdYU8y0J9YBcMEgkx5zKec/s827/Resume3.jpg" alt='_Resume3' className='online' target='_blank' />
   </Link>
- 
-  <Link to="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEii5ueqHIuqUh2R7gfoc_RYJ4gRhFokI1P9l4XNvnl2YQba_aB5afTIItIij8PyBs9eS4R2acgHRYoOKqeZhD3GddvK3roBgxhPZoc9KCap3eX23TMydwmH9VqrHX3yN8udxySTKZXw02U6bTsxRhUMZTdZQZmZHfa7aA7z_45xWSz3MOW3Pi_Ijj8ShGI/s826/Resume2.jpg" alt='_Resume2' className='online' target='_blank' >
-  <h1>OPEN
-  <h3>Resume-Slide
-  <h4>2/3</h4><br/>
-  </h3><br/>
-  </h1><br/>
-  </Link>
-
-  <Link to="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg_TYRGqnt4Lda6WLMGVA6cmxvxjyEXWmiqVRxk6q7MVyYe8xrp9oy2dQerrIhgPXe8nqx1moYaR3VK9ZczNhuC8m0sqCq6Apl-DfTgF54Aj1sp93bqvkz0W2_z5P-s9Od_FnLrhT5KRj5Vx82VKh1xawk7iguNNPyw-owR4LdYU8y0J9YBcMEgkx5zKec/s827/Resume3.jpg" alt='_Resume3' className='online' target='_blank' >
-  <h1>OPEN
-  <h3>Resume-Slide
-  <h4>3/3</h4><br/>
-  </h3><br/>
-  </h1><br/>
-  </Link>
-
-</>
+ </>
   )
 }
 
